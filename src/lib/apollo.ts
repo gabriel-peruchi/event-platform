0c1ba38dd31694efd3845443902097d3dcd43cde
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2022-06-24 22:01:50 -0300

    feat: subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oveslj178p01xs62w5681s/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: { 'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` },
   cache: new InMemoryCache()
 })
